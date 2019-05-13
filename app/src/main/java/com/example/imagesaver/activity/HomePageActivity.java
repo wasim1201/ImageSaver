fabdbfc91269d87f54023830d916aa97aa4046a0
Sonu Saini <sonu@zaax.com>
2019-05-13 15:11:29 +0530

    Adding custom chrome tabs

--- app/src/main/java/com/example/imagesaver/activity/HomePageActivity.java
+++ app/src/main/java/com/example/imagesaver/activity/HomePageActivity.java
@@ -1,9 +1,12 @@
 package com.example.imagesaver.activity;
 
 import android.content.Intent;
-import android.support.design.widget.TextInputLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
@@ -17,13 +20,14 @@ import com.example.imagesaver.Util.ImageSaverPreferences;
 public class HomePageActivity extends AppCompatActivity {
 
 
-    private static final String TAG = HomePageActivity.class.getSimpleName() ;
+    private static final String TAG = HomePageActivity.class.getSimpleName();
 
     private String mName, mLocation;
-    private  EditText mETName, mETLocation;
+    private EditText mETName, mETLocation;
     private Toolbar mToolbar;
     private TextInputLayout nameLayout;
     private TextInputLayout locationLayout;
+    private TextView termsAndCondition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +42,11 @@ public class HomePageActivity extends AppCompatActivity {
 
     private void initViews() {
 
-        mETName= findViewById(R.id.enter_name);
-        mETLocation= findViewById(R.id.enter_location);
+        mETName = findViewById(R.id.enter_name);
+        mETLocation = findViewById(R.id.enter_location);
         nameLayout = findViewById(R.id.layout_enter_name);
         locationLayout = findViewById(R.id.layout_enter_location);
+        termsAndCondition = findViewById(R.id.termsCondition);
 
         TextView submit = findViewById(R.id.submit);
         initCredentials();
@@ -50,10 +55,17 @@ public class HomePageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(isValidCredentials(mETName.getText().toString().trim(), mETLocation.getText().toString().trim())){
-                    ImageSaverPreferences.setPreferencesNameLocation(HomePageActivity.this, mETName.getText().toString().trim(), mETLocation.getText().toString().trim() );
+                if (isValidCredentials(mETName.getText().toString().trim(), mETLocation.getText().toString().trim())) {
+                    ImageSaverPreferences.setPreferencesNameLocation(HomePageActivity.this, mETName.getText().toString().trim(), mETLocation.getText().toString().trim());
                     startActivity(new Intent(HomePageActivity.this, SupplierActivity.class));
                 }
+            }
+        });
+
+        termsAndCondition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectUsingCustomTab("https://github.com/wasim1201");
             }
         });
 
@@ -66,33 +78,44 @@ public class HomePageActivity extends AppCompatActivity {
         }
     }
 
+
+    private void redirectUsingCustomTab(String url) {
+        Uri uri = Uri.parse(url);
+        CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
+        intentBuilder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        intentBuilder.setSecondaryToolbarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        CustomTabsIntent customTabsIntent = intentBuilder.build();
+        customTabsIntent.launchUrl(this, uri);
+    }
+
+
     private void initCredentials() {
         getPrefNameLocation();
 
-        if(! TextUtils.isEmpty(mName) && mName != null && ! TextUtils.isEmpty(mLocation) && mLocation != null){
+        if (!TextUtils.isEmpty(mName) && mName != null && !TextUtils.isEmpty(mLocation) && mLocation != null) {
             mETName.setText(mName);
             mETLocation.setText(mLocation);
         }
     }
 
     private void getPrefNameLocation() {
-        mName= ImageSaverPreferences.getPreferencesName(this);
-        mLocation= ImageSaverPreferences.getPreferenceLocation(this);
+        mName = ImageSaverPreferences.getPreferencesName(this);
+        mLocation = ImageSaverPreferences.getPreferenceLocation(this);
         Log.d(TAG, "getPrefNameLocation: " + mName + " - " + mLocation);
     }
 
-    private boolean isValidCredentials(String name, String location){
-        if(TextUtils.isEmpty(name)){
+    private boolean isValidCredentials(String name, String location) {
+        if (TextUtils.isEmpty(name)) {
             nameLayout.setErrorEnabled(true);
             nameLayout.setError("Enter the name");
-        }else{
+        } else {
             nameLayout.setErrorEnabled(false);
         }
 
-        if(TextUtils.isEmpty(location)){
+        if (TextUtils.isEmpty(location)) {
             locationLayout.setErrorEnabled(true);
             locationLayout.setError("Enter the location");
-        }else{
+        } else {
             locationLayout.setErrorEnabled(false);
         }
 
